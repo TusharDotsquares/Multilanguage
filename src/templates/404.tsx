3f7ce0da336c8363efc927f38c8c46a63873c7ad
Tushar <tushar.ruhela@dostquares.com>
2023-06-29 18:08:55 +0530

    update 404 page

--- src/templates/404.tsx
+++ src/templates/404.tsx
@@ -13,7 +13,8 @@ import PageLayout from "../components/layout/PageLayout";
 import favicon from "../assets/images/favicon.ico";
 import { TemplateMeta } from "../types";
 import { FourOhFourDocument } from "../types/Locator";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StaticRouter } from "react-router-dom/server";
 export const config: TemplateConfig = {
   name: "404",
 };
@@ -45,21 +46,31 @@ export const getHeadConfig: GetHeadConfig<
 interface FourOhFourProps extends TemplateRenderProps {
   __meta: TemplateMeta;
   document: FourOhFourDocument;
+  path:string
 }
-
+const Router = typeof document !== "undefined" ? BrowserRouter : StaticRouter;
 const FourOhFour: Template<FourOhFourProps> = ({
   __meta,
   document,
+  path
 }: FourOhFourProps) => {
+ const url = "404.html";
   return (
     <>
-      <PageLayout _site={document._site} meta={__meta}>
-        <div className="centered-container">
-          <div className="flex justify-center items-center text-2xl bg-gray-200 h-60 rounded-md shadow-md">
-            <p>This page does not exist.</p>
-          </div>
-        </div>
-      </PageLayout>
+  <Router location={url}>
+      <Routes>
+        <Route
+          path={url}
+          element={
+          <PageLayout _site={document._site} meta={__meta} path={path}>
+            <div className="centered-container">
+              <div className="flex justify-center items-center text-2xl bg-gray-200 h-60 rounded-md shadow-md">
+                <p>This page does not exist.</p>
+              </div>
+            </div>
+          </PageLayout>}/>
+      </Routes>
+   </Router>
     </>
   );
 };
